d5f9929d267cafa81e6ace7158dd674153b2c20f
adityay04 <adityayadav0304@gmail.com>
2026-08-14 16:39:11 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if (n == 0) return true;
+        int i = 0;
+        for(int j = 0; j<m; j++){
+            if(s[i]==t[j]){
+                i++;
+                if (i == n) return true;
+            }
+        }
+        return false;
+    }
+};
